14d8acd19f4dce5686e114fccd478120aeb3e37d
Geoff Greer <geoff@greer.fm>
2012-06-14 00:37:42 -0700

    Use a context menu instead of a browser action

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
+chrome.contextMenus.create({
+  title: "Open all links in selection",
+  contexts: ["selection"],
+  onclick: function (tab) {
     chrome.tabs.executeScript(null, {file: "content_script.js"});
+  }
 });
